4f3543b01c2204e92f9001440d94d5f7033ed61d
Ksenia_Filatova <tatarkina.kcenia@yandex.ru>
2025-01-12 17:24:25 +0300

    First commit

---
+++ home_work/5_hw.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get('https://www.saucedemo.com/')
+
+
+field_1 = driver.find_element(By.CSS_SELECTOR, '#user-name')
+if field_1 is None:
+    print('Элемент не найден')
+else:
+    print('Элемент найден')
+
+field_2 = driver.find_element(By.CSS_SELECTOR, '#password')
+if field_2 is None:
+    print('Элемент не найден')
+else:
+    print('Элемент найден')
+
+btn = driver.find_element(By.CSS_SELECTOR, '#login-button')
+if btn is None:
+    print('Элемент не найден')
+else:
+    print('Элемент найден')
+
+
+
+
